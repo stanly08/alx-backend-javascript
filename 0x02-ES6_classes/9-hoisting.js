5a73d4e5788ce27a9654a7667d3883d5dc9e58e6
stanly08 <anasistanly@gmail.com>
2024-07-03 20:30:07 +0000

    classes

---
+++ 0x02-ES6_classes/9-hoisting.js
@@ -0,0 +1,22 @@
+export class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
+
+export class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
