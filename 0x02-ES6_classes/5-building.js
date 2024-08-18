315d48a8a55d14bc49869fddbc1548d7de288da5
stanly08 <anasistanly@gmail.com>
2024-08-18 21:07:10 +0000

    npm

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,16 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    console.log(this.#evacuationWarningMessage())
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  set sqft(value) {
+    this._sqft = value;
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  set sqft(sqft) {
-    this._sqft = sqft;
-  }
-
- #evacuationWarningMessage(){
-  return 'Hey There'
- }
 }
